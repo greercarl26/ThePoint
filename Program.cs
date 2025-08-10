fb3db850902c8e4bd0626517301cded71788cee7
greercarl26 <greercarl26@gmail.com>
2025-08-09 21:24:52 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Point point1 = new Point(2, 3);
+Point point2 = new Point(-4, 0);
+
+Console.WriteLine($"({point1.X},{point1.Y})");
+Console.WriteLine($"({point2.X},{point2.Y})");
+
+
+class Point
+{
+    // Feilds
+    private float _x;
+    private float _y;
+
+    //Constrctors
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+    }
+    //Properties
+    public float X
+    {
+        get => _x;
+        set => _x = value;
+
+     }
+    public float Y
+    {
+        get => _y;
+        set => _y = value;
+
+     }
+
+}
